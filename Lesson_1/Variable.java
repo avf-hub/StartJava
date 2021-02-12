df8432228da6d7554a237be6cbb3ed1d38f7ac2a
Andrey Falkovskiy <avf-91@yandex.ru>
2021-02-12 17:11:19 +0300

    Создайте класс Variable - ДЗ №1 (примитивные типы)

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,21 @@
+class Variable {
+	public static void main(String[] args) {
+		byte processor = 1;
+		short core = 4;
+		int year = 2011;
+		long bitness = 64;
+		float frequency = 2.80f;
+		double memory = 8.0;
+		char symbol = 'x';
+		boolean laptop = true;
+
+		System.out.println("Information about my laptop:");
+		System.out.println("Number of processors - " + processor);
+		System.out.println("Processor production year - " + year);
+		System.out.println("Number of сores - " + core);
+		System.out.println("Operating system Windows 10 bitness - " + symbol + bitness);
+		System.out.println("CPU core frequency - " + frequency + " GHz");
+		System.out.println("RAM size - " + memory + " Gb");
+		System.out.println("My laptop is cool? - " + laptop);	
+	}
+} 
